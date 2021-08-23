0bca8cfda9c2b63a370c443eb8b93488e601b2af
Victor Huberta <victorhuberta@gmail.com>
2021-08-23 11:00:16 +0700

    Add game modes.

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,75 @@
 
 use bracket_lib::prelude::*;
 
-struct State {}
+struct State {
+    mode: GameMode,
+}
+
+impl State {
+    fn new() -> Self {
+        Self {
+            mode: GameMode::MainMenu,
+        }
+    }
+
+    fn main_menu(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "Flappy Dragon");
+        ctx.print_centered(8, "(P) Start Playing");
+        ctx.print_centered(9, "(Q) Quit Game");
+
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
+    }
+
+    fn play(&mut self, _ctx: &mut BTerm) {
+        self.mode = GameMode::GameOver;
+    }
+
+    fn restart(&mut self) {
+        self.mode = GameMode::Playing;
+    }
+
+    fn game_over(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "YOU ARE DEAD");
+        ctx.print_centered(8, "(P) Play Again");
+        ctx.print_centered(9, "(Q) Quit Game");
+
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
+    }
+}
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
-        ctx.cls();
-        ctx.print(1, 1, "Hello, Flappy Bird!");
+        match &self.mode {
+            GameMode::MainMenu => self.main_menu(ctx),
+            GameMode::Playing => self.play(ctx),
+            GameMode::GameOver => self.game_over(ctx),
+        }
     }
+}
+
+enum GameMode {
+    MainMenu,
+    Playing,
+    GameOver,
 }
 
 fn main() -> BError {
     let ctx = BTermBuilder::simple80x50()
         .with_title("Flappy Dragon")
         .build()?;
-    main_loop(ctx, State {})
+    main_loop(ctx, State::new())
 }
